459e52eba502eba41ca31c17dc79cd8463f87d78
khizaryasin <mkhizaryasin123@gmail.com>
2022-01-07 17:08:09 +0500

    append function

--- manager.js
+++ manager.js
@@ -1,1 +1,34 @@
+const append = document.getElementById('append');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const grid = document.getElementById('grid');
+const col = document.querySelector('.col')
+
+
+form.addEventListener('submit', (wow)=>{
+wow.preventDefault();
+
+addProject();
+})
+
+function addProject(){
+    
+    if(grid){
+        const projectElement =  $(grid).append(`<div class ='col-md-3'><div id ='newProject'></div></div>`)
+    }
+    
+}
+
+function projectName(){
+    const projectName = input.value;
+
+}
+
+
+
+
+
+addProject();
+
+
 
